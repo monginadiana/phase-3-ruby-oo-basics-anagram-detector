295de594ec36272a2fc3944d491efa66491a5d79
monginadiana <dianahmongina2@gmail.com>
2022-09-29 16:23:04 +0300

    commit tree b8c2f926225e50316747c55d8d981ce14015d917

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word=word
+    end
+    def match(words)
+        @words =words
+        letter= @words.map {|word| word.chars.sort.join}
+        crtword= @word.chars.sort.join
+        arr = []
+        letter.each_with_index do|wrd,i|
+            if wrd==crtword
+                arr.push(i)
+            end
+        end
+    end
+end
